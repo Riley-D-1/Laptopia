a7b579966d954aa1ed65ca2a27ca2c92619e1f40
Riley-D-1 <171879480+Riley-D-1@users.noreply.github.com>
2025-11-22 22:12:52 +1100

    Working on the web scraper
    Got it to scrape all of the links now working on scraping for specs and price.

--- scraper.py
+++ scraper.py
@@ -1,27 +1,45 @@
-import requests
-import pandas
-from bs4 import BeautifulSoup
-from requests_html import HTMLSession
-headers = {"User-Agent": "Mozilla/5.0"}
-delay = 1000 # 1 second in  ms
+# Imports
+import pandas as pd
+import time 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+# Setting up info
 jb_core_url = "https://www.jbhifi.com.au/collections/computers-tablets/laptops?hitsPerPage=400"
-# Class fo url links class="page-identifier"
-# class again- ProductCard _10ipotx28 _10ipotx27 _10ipotx2b
-# Spec sheet class = _14wuv360
-def jb_map_scrape(url):
-    page = requests.get(url,headers=headers)
-    soup = BeautifulSoup(page.content, "html.parser")
+delay = 1000 # 1 second in  ms
+
+def jb_map_scrape(url,delay):
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get(url)
+    driver.implicitly_wait(delay)
     links = []
-    print(soup)
-    print("test")
-    for val in soup.find_all('div', class_='ProductCard _10ipotx28 _10ipotx27 _10ipotx2b'):
-        print("card found")
-        links.append("https://www.jbhifi.com.au" + val["href"])
+    cards = driver.find_elements(By.CSS_SELECTOR, "a[href^='/products/']")
+    for card in cards:
+        href = card.get_attribute("href")
+        links.append(href)
+    driver.quit()
+    print(f"{len(links)} Products found")
     return links
-def product_scrape(links):
+
+def product_scrape(links,delay):
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     for link in links:
-        print()
-def main():
-    print()
-    
-print(jb_map_scrape(jb_core_url))
+        driver.get(link)  
+        driver.implicitly_wait(delay)
+        try:
+            print("trying to find stuff")
+            specs = driver.find_elements(By.CSS_SELECTOR, "div._14wuv360")
+        except:
+            print("Error, spec not found ")
+            pass
+        # To ensure I dont break terms of service and be a good web scraper
+        time.sleep(3)
+
+
+def main(jb_core_url,delay):
+    links = jb_map_scrape(jb_core_url,delay)
+    scraped_specs = product_scrape(links,delay)
+    df = pd.DataFrame(scraped_specs)
+
+main(jb_core_url,delay)
